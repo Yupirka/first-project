2ab6a9de3e6c0d3f493801e5e86e340232dc8ed6
Zakhar <example@gmail.com>
2023-12-26 18:57:45 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,13 @@
+"use strict"
+let incr = 10,
+    decr = 10;
+// ++incr;
+// --decr;
+console.log (++incr)
+console.log (--decr)
+console.log (6*7 !== 42)
+console.log (8%3)
+console.log (`8*3` === 24)
+const isChecked = false,
+      Close = false;
+console.log (isChecked || Close)
